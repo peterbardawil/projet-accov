ca7e8a96c15e991c2faeea265eb2d4e4cec2b0c1
Peter Bardawil <peterbardawil@gmail.com>
2018-08-20 22:25:51 +0300

    ajouter la deuxieme solution avec la nouvelle class cameneon et la serveur etoile qui veut gerer les cameneon

---
+++ Accov/src/accov-solution2/ServeurEtoile.java
@@ -0,0 +1,16 @@
+package accov2;
+
+/**
+ * <h1>ServeurEtoile Class</h1>
+ * Ce class est pour gerer tout les cameneon qui veulent connecter 
+ * pour faire la mutation avec les cameneon qui sont connecter avec le serveur
+ *
+ * @author Peter Bardawil
+ */
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServeurEtoile {
+
+}

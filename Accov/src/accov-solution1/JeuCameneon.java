c7163b617b21539e8365879c83f854580157c05d
Peter Bardawil <peter.bardawil@cleartag.local>
2018-08-24 17:48:46 +0300

    changing info

--- Accov/src/accov-solution1/JeuCameneon.java
+++ Accov/src/accov-solution1/JeuCameneon.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package accov;
 
 /**
  *
- * @author bachir
+ * @author Peter Bardawil
  */
 public class JeuCameneon {
     //Groupe De Couleur De Tout Cameneon
